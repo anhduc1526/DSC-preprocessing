ab46ff75b5c26cc20eabb743352359ddb96bb59c
trantran <camtran3506@gmail.com>
2026-08-19 14:21:18 +0700

    Data analysis

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from md_to_json import build_segments_json
 from preprocess import process_json
 
 if __name__ == "__main__":
-    Path("processed-contexts").mkdir(parents=True, exist_ok=True)
+    Path("/mnt/mmlab2024nas/trantran/processed-contexts").mkdir(parents=True, exist_ok=True)
 
-    for file_name in glob("selected-contexts/*"):
+    for file_name in glob("/mnt/mmlab2024nas/trantran/selected-contexts/*"):
         input_file = Path(file_name)
 
         print(f"Đang xử lý: {input_file.name}")
 
         process_json(
             file_name,
-            "processed.md",
+            "/mnt/mmlab2024nas/trantran/processed-contexts/processed.md",
         )
 
         build_segments_json(
             file_name,
-            "processed.md",
-            f"processed-contexts/{input_file.name}",
+            "/mnt/mmlab2024nas/trantran/processed-contexts/processed.md",
+            f"/mnt/mmlab2024nas/trantran/processed-contexts/{input_file.name}",
         )
